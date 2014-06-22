a7d8e60c2a5074ed13f88f767af99eadfc614aa2
miramzz <mira.muazzez@gmail.com>
2014-06-22 14:40:32 -0700

    templates added, related test cases and code added

--- journal.py
+++ journal.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
+import datetime
 import psycopg2
 from contextlib import closing
 from flask import Flask
 from flask import g
+from flask import render_template
 
 
 
@@ -18,12 +20,39 @@ CREATE TABLE entries (
 )
 """
 
+DB_ENTRY_INSERT = """
+INSERT INTO entries (title, text, created) VALUES (%s, %s, %s)
+"""
+
+DB_ENTRIES_LIST = """
+SELECT id, title, text, created FROM entries ORDER BY created DESC
+"""
+
 app = Flask(__name__)
 
 
+def write_entry(title, text):
+    if not title or not text:
+        raise ValueError("Title and text required for writing an entry")
+    con = get_database_connection()
+    cur = con.cursor()
+    now = datetime.datetime.utcnow()
+    cur.execute(DB_ENTRY_INSERT, [title, text, now])
+
+
+def get_all_entries():
+    """return a list of all entries as dicts"""
+    con = get_database_connection()
+    cur = con.cursor()
+    cur.execute(DB_ENTRIES_LIST)
+    keys = ('id', 'title', 'text', 'created')
+    return [dict(zip(keys, row)) for row in cur.fetchall()]
+
+
 @app.route('/')
-def hello():
-    return u'Hello world!'
+def show_entries():
+    entries = get_all_entries()
+    return render_template('list_entries.html', entries=entries)
 
 
 # letting application know about how to connect db
---
+++ test_journal.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from contextlib import closing
+import pytest
+
+from journal import app
+from journal import connect_db
+from journal import get_database_connection
+from journal import init_db
+
+
+TEST_DSN = 'dbname=test_learning_journal user=muazzezmira'
+
+
+def clear_db():
+    with closing(connect_db()) as db:
+        db.cursor().execute("DROP TABLE entries")
+        db.commit()
+
+
+@pytest.fixture(scope='session')
+def test_app():
+    """configure our app for use in testing"""
+    app.config['DATABASE'] = TEST_DSN
+    app.config['TESTING'] = True
+
+
+@pytest.fixture(scope='session')
+def db(test_app, request):
+    """initialize the entries table and drop it when finished"""
+    init_db()
+
+    def cleanup():
+        clear_db()
+
+    request.addfinalizer(cleanup)
+
+
+@pytest.yield_fixture(scope='function')
+def req_context(db):
+    """run tests within a test request context so that 'g' is present"""
+    with app.test_request_context('/'):
+        yield
+        con = get_database_connection()
+        con.rollback()
+
+
+@pytest.fixture(scope='function')
+def with_entry(db, request):
+    from journal import write_entry
+    expected = (u'Test Title', u'Test Text')
+    with app.test_request_context('/'):
+        write_entry(*expected)
+        # manually commit transaction here to avoid rollback due to
+        # handled exceptions
+        get_database_connection().commit()
+
+    def cleanup():
+        with app.test_request_context('/'):
+            con = get_database_connection()
+            cur = con.cursor()
+            cur.execute("DELETE FROM entries")
+            # and here as well
+            con.commit()
+    request.addfinalizer(cleanup)
+
+    return expected
+
+
+def run_independent_query(query, params=[]):
+    con = get_database_connection()
+    cur = con.cursor()
+    cur.execute(query, params)
+    return cur.fetchall()
+
+
+def test_write_entry(req_context):
+    from journal import write_entry
+    expected = ("My Title", "My Text")
+    write_entry(*expected)
+    rows = run_independent_query("SELECT * FROM entries")
+    assert len(rows) == 1
+    for val in expected:
+        assert val in rows[0]
+
+
+def test_get_all_entries_empty(req_context):
+    from journal import get_all_entries
+    entries = get_all_entries()
+    assert len(entries) == 0
+
+
+def test_get_all_entries(req_context):
+    from journal import get_all_entries, write_entry
+    expected = ("My Title", "My Text")
+    write_entry(*expected)
+    entries = get_all_entries()
+    assert len(entries) == 1
+    for entry in entries:
+        assert expected[0] == entry['title']
+        assert expected[1] == entry['text']
+        assert 'created' in entry
+
+
+def test_empty_listing(db):
+    actual = app.test_client().get('/').data
+    expected = 'No entries here so far'
+    assert expected in actual
+
+
+def test_listing(with_entry):
+    expected = with_entry
+    actual = app.test_client().get('/').data
+    for value in expected:
+        assert value in actual
