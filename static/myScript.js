32b200b291fb438b6dd2b7f74b89cf166e50aa36
miramzz <mira.muazzez@gmail.com>
2014-07-28 19:09:18 -0700

    update entries added

--- static/myScript.js
+++ static/myScript.js
@@ -4,12 +4,27 @@ $(function(){
         $.ajax("/add", {
             type:'POST',
             data:$('form').serialize(),
-            success: function(entryData){
-              entryData.preventDefault();
+            success: function(show_entries){
+              show_entries.preventDefault();
               $('.entry').html(entryData);
               $('.add_entry').remove();
             }
         });
+    });
+});
+
+$(function(){
+    $(".edit").on('submit', function(updateEntry){
+        updateEntry.preventDefault();
+        $.ajax("/edit/{{ entry[0] }}", {
+            type:'POST',
+            data:$('form').serialize(),
+            success: function(entryData){
+              entryData.preventDefault(),
+              $('.edit').html(updateEntry);
+              $('.edit_entry').remove();
+            }
+        })
     });
 });
 
